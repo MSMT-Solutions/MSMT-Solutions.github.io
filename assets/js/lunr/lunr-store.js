6193a1bb6cccab3170b47057b5e4b1f6f10751df
henning <henning@msmt.solutions>
2023-05-29 00:03:03 +0800

    Built site locally

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Instrumentation cable",
+        "excerpt":"Instrumentation Cable At MSMT Solutions, we recognise the importance of utilising high-quality components, including reliable cables, in various applications. We understand that cables must not only withstand environmental conditions but also be robust and flexible enough to meet practical needs. Drawing upon our extensive experience in research, we proudly offer...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/cable/",
+        "teaser": "http://localhost:4000/assets/images/product-images/5_cable.jpg"
+      },{
+        "title": "Shear Vane Series",
+        "excerpt":"Porous Ceramic Discs At MSMT Solutions, we offer a diverse range of porous ceramic discs in various sizes and thicknesses. These discs are meticulously crafted with a strong emphasis on detail and quality. Our porous ceramics exhibit exceptional durability, making them capable of withstanding rigorous mechanical installation. Additionally, our precision...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/ceramic_filter/",
+        "teaser": "http://localhost:4000/assets/images/product-images/4_filter_size.jpg"
+      },{
+        "title": "Penetrometer Series",
+        "excerpt":"Miniature Penetrometer The miniature penetrometer series is specifically designed to investigate soil strength profiles in either centrifuge or laboratory equipment to a very high precision. The product offers an exceptionally miniaturised design with a 5 mm shaft and three different penetrometer tips: (i) D5 mm Cone, (ii) D5 x 20...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/penetrometer/",
+        "teaser": "http://localhost:4000/assets/images/product-images/1_Penetrometer_landing.jpg"
+      },{
+        "title": "Shear Vane Series",
+        "excerpt":"Porous Ceramic Discs At MSMT Solutions, we offer a diverse range of porous ceramic discs in various sizes and thicknesses. These discs are meticulously crafted with a strong emphasis on detail and quality. Our porous ceramics exhibit exceptional durability, making them capable of withstanding rigorous mechanical installation. Additionally, our precision...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/plastic_filter/",
+        "teaser": "http://localhost:4000/assets/images/product-images/4_filter_size.jpg"
+      },{
+        "title": "Pressure Transducer Series",
+        "excerpt":"Miniature Pressure Transducer The miniature pressure sensor series is specifically designed to investigate differential pressures within the range of 100 kPa. They are commonly used in geotechnical or hydrodynamical applications to measure pore pressure or fluid pressure (static or dynamic), respectively. The product offers two exceptional miniature designs with the...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/pressure/",
+        "teaser": "http://localhost:4000/assets/images/product-images/1_PPT_landing.jpg"
+      },{
+        "title": "Shear Vane Series",
+        "excerpt":"Miniature Shear Vane The Shear Vane series is specifically designed to accurately measure the undrained shear strength of cohesive soils. This type of test involves inserting a vane into the soil and measuring the torque required to rotate it, providing a reliable indication of the soil’s shear strength. The Shear...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/vane/",
+        "teaser": "http://localhost:4000/assets/images/product-images/3_vane_landing.jpg"
+      },]
